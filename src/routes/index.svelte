96b2b9263209107b63ccca9ec81b1e248af8097b
cryptodeal <jimmydeal16@gmail.com>
2021-05-15 07:14:40 -0500

    test optimization of home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,10 @@
 <script context="module">
+  import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+
+	export const router = browser;
+
+	export const prerender = true;
 	/**
 	 * @type {import('@sveltejs/kit').Load}
 	 */
