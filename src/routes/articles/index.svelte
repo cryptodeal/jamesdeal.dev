663bc8fb4ef29e38d96ba59787428ea319577aa9
cryptodeal <jimmydeal16@gmail.com>
2021-05-16 14:06:52 -0500

    optimize load time of articles

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -1,4 +1,8 @@
 <script context="module">
+	import { browser, dev } from '$app/env';
+	export const hydrate = dev;
+	export const router = browser;
+	export const prerender = true;
   export async function load({ fetch }) {
     const url = `/articles.json`;
     const res = await fetch(url);
