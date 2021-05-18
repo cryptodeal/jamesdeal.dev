d67fe62320c78d096836d71ccf4f8bc890752248
cryptodeal <jimmydeal16@gmail.com>
2021-05-17 20:24:19 -0500

    standardize css; lint and prettier

--- src/lib/about/BulletedList.svelte
+++ src/lib/about/BulletedList.svelte
@@ -2,7 +2,7 @@
   export let bulletItems;
 </script>
 <ul
-  class="font-extralight list-disc list-inside text-left text-sm text-gray-800 w-3/4 inline-block items-center sm:w-1/2 md:w-3/4 dark:text-gray-200">
+  class="font-extralight font-mono list-disc list-inside text-left text-sm text-gray-800 w-3/4 inline-block items-center sm:w-1/2 md:w-3/4 dark:text-gray-200">
   {#each bulletItems as itemText}
   {#if Array.isArray(itemText)}
   <li>{itemText[0]} <a href={itemText[1][1]} target="_blank" rel="noopener noreferrer">{itemText[1][0]}</a></li>
