b3679e3b59cd952223386fb57d3fc95112688978
cryptodeal <jimmydeal16@gmail.com>
2021-05-19 20:12:30 -0500

    fix article typography theme/style

--- src/routes/articles/read/__layout.svelte
+++ src/routes/articles/read/__layout.svelte
@@ -2,7 +2,7 @@
   <meta property="og:type" content="article" />
 </svelte:head>
 <div class="mx-auto max-w-3xl py-10 px-4 sm:px-6 sm:py-12 lg:max-w-4xl lg:py-16 lg:px-8 xl:max-w-6xl">
-  <article class="mx-auto prose prose-sm sm:prose lg:prose-xl xl:prose-2xl">
+  <article class="mx-auto prose prose-sm sm:prose lg:prose-xl 2xl:prose-2xl">
     <slot />
   </article>
   <div class="flex py-1 twitter justify-center">
--- windi.config.js
+++ windi.config.js
@@ -5,6 +5,34 @@ const windiConfig = {
 		enableAll: true
 	},
 	theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#27272a',
+            a: {
+              textDecoration: 'underline',
+              'color': '#991b1b',
+              '&:hover': {
+                color: '#7f1d1d',
+              },
+            },
+            h1: {
+              color: '#991b1b',
+            },
+            h2: {
+              color: '#991b1b',
+            },
+            h3: {
+              color: '#991b1b',
+            },
+            h4: {
+              color: '#991b1b',
+            },
+          },
+        },
+      },
+    },
 		boxShadow: {
 			animate:
 				'0.5px 0.5px rgba(255,255,255,1), 1.5px 1.5px rgba(255,255,255,1), 2.5px 2.5px rgba(255,255,255,1), 3.5px 3.5px rgba(255,255,255,1), 4.5px 4.5px rgba(255,255,255,1)'
