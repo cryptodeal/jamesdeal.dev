4b3f437191885b76b410c0c0b1f51d7e00622434
cryptodeal <jimmydeal16@gmail.com>
2021-05-27 20:26:34 -0500

    fix mongo error on vercel deployment

--- src/lib/_db/models.js
+++ src/lib/_db/models.js
@@ -1,38 +1,42 @@
 import mongoose from 'mongoose';
+import initConnection from '$lib/_db/initConnect';
 const Schema = mongoose.Schema;
+var Commit;
+var Repo;
 
-let Commit;
-try {
-	Commit = mongoose.model('Commit');
-} catch (error) {
-	Commit = mongoose.model(
-		'Commit',
-		new Schema({
-			_id: { type: String },
-			author: {
-				email: { type: String, require: true },
-				name: { type: String, require: true }
-			},
-			message: { type: String, require: true },
-			distinct: { type: Boolean, require: true },
-			repo: { type: Number, ref: 'Repo', index: true },
-			date: { type: Date, require: true }
-		})
-	);
-}
-
-let Repo;
-try {
-	Repo = mongoose.model('Repo');
-} catch (error) {
-	Repo = mongoose.model(
-		'Repo',
-		new Schema({
-			_id: { type: Number },
-			name: { type: String, require: true, index: true },
-			url: { type: String, require: true }
-		})
-	);
-}
+initConnection()
+  .then(()=> {
+    try {
+      Commit = mongoose.model('Commit');
+    } catch (error) {
+      Commit = mongoose.model(
+        'Commit',
+        new Schema({
+          _id: { type: String },
+          author: {
+            email: { type: String, require: true },
+            name: { type: String, require: true }
+          },
+          message: { type: String, require: true },
+          distinct: { type: Boolean, require: true },
+          repo: { type: Number, ref: 'Repo', index: true },
+          date: { type: Date, require: true }
+        })
+      );
+    }
+    
+    try {
+      Repo = mongoose.model('Repo');
+    } catch (error) {
+      Repo = mongoose.model(
+        'Repo',
+        new Schema({
+          _id: { type: Number },
+          name: { type: String, require: true, index: true },
+          url: { type: String, require: true }
+        })
+      );
+    }
+  })
 
 export { Commit, Repo };
