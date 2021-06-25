8245d6ed2687200a9e1e6b0e23232939c9c27bd3
cryptodeal <jimmydeal16@gmail.com>
2021-06-24 21:26:24 -0500

    fix styles

--- src/routes/demos/index.svelte
+++ src/routes/demos/index.svelte
@@ -47,7 +47,7 @@
 	<title>Demos</title>
 </svelte:head>
 
-<h1 class="my-5">Demos</h1>
+<h1 class="font-light font-sans my-5 text-center text-5xl text-red-800 break-words md:text-7xl dark:text-green-400">Demos</h1>
 <div class="flex mb-5">
 	<nav class="mx-auto h-8 gap-20 inline-flex">
 		<svg
@@ -63,7 +63,7 @@
 				d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm2 12l-4.5 4.5 1.527 1.5 5.973-6-5.973-6-1.527 1.5 4.5 4.5z"
 			/>
 		</svg>
-		<h2 class="self-center">Data Viz</h2>
+		<h2 class="font-extralight font-sans text-2xl text-red-800 self-center lg:text-4xl dark:text-green-400">Data Viz</h2>
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			on:click={nextChart}
