80901bf92dac970999739dda8c091eff76cae900
cryptodeal <jimmydeal16@gmail.com>
2021-07-03 18:46:02 -0500

    add saslprep as dep and include in optimizeDeps; testing vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
 				}
 			},
 			optimizeDeps: {
-				include: ['layercake', 'mongoose']
+				include: ['layercake', 'saslprep']
 			},
 			plugins: [WindiCSS.default(), imagetools()]
 		}
