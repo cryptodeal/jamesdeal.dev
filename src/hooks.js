66f9c8885cc6de4f9f46abfb8c30fb632a7a7e65
cryptodeal <jimmydeal16@gmail.com>
2021-09-19 19:55:01 -0500

    update deps

--- src/hooks.js
+++ src/hooks.js
@@ -17,7 +17,7 @@ export const handle = async ({ request, resolve }) => {
 
 	if (!cookies.userid) {
 		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
+		// set a cookie so that we recognize them when they return
 		response.headers['set-cookie'] = `userid=${request.locals.userid}; Path=/; HttpOnly`;
 	}
 
